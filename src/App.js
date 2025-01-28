1e970b3ad8a2136550ee1f6c58ac9023782bb143
onlysea2704 <phamduyhai2704@gmail.com>
2025-01-28 22:59:31 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header_1/Header';
-import Home from './components/Home_1/Home';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Services from './components/Services/Services';
